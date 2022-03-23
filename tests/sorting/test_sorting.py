0514a235e7bc4301cc72d72b043f7b49ec27d95c
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-03-23 20:19:32 -0300

    test_sorting file with 3 xfailed & 1 passed test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,23 +1,44 @@
+import pytest
 from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    fake_criteria = 'min_salary'
     fake_jobs = [
-        {'min_salary': 1, 'max_salary': 10},
-        {'min_salary': 2, 'max_salary': 20},
-        {'min_salary': 4, 'max_salary': 40},
-        {'min_salary': 3, 'max_salary': 30},
+        {'min_salary': 2, 'max_salary': 20, 'date_posted': '2011-01-01'},
+        {'min_salary': 1, 'max_salary': 10, 'date_posted': '2010-01-01'},
+        {'min_salary': 4, 'max_salary': 40, 'date_posted': '2013-01-01'},
+        {'min_salary': 3, 'max_salary': 30, 'date_posted': '2012-01-01'},
     ]
 
-    expected_jobs = [
-        {'min_salary': 1, 'max_salary': 10},
-        {'min_salary': 2, 'max_salary': 20},
-        {'min_salary': 3, 'max_salary': 30},
-        {'min_salary': 4, 'max_salary': 40},
+    expected_min = [
+        {'min_salary': 1, 'max_salary': 10, 'date_posted': '2010-01-01'},
+        {'min_salary': 2, 'max_salary': 20, 'date_posted': '2011-01-01'},
+        {'min_salary': 3, 'max_salary': 30, 'date_posted': '2012-01-01'},
+        {'min_salary': 4, 'max_salary': 40, 'date_posted': '2013-01-01'},
     ]
 
-    assert sort_by(fake_jobs, fake_criteria) == expected_jobs
+    expected_max = [
+        {'min_salary': 4, 'max_salary': 40, 'date_posted': '2013-01-01'},
+        {'min_salary': 3, 'max_salary': 30, 'date_posted': '2012-01-01'},
+        {'min_salary': 2, 'max_salary': 20, 'date_posted': '2011-01-01'},
+        {'min_salary': 1, 'max_salary': 10, 'date_posted': '2010-01-01'},
+    ]
 
-    fake_criteria = 'wrong'
-    assert sort_by(fake_jobs, fake_criteria) == ValueError
+    expected_date = [
+        {'min_salary': 4, 'max_salary': 40, 'date_posted': '2013-01-01'},
+        {'min_salary': 3, 'max_salary': 30, 'date_posted': '2012-01-01'},
+        {'min_salary': 2, 'max_salary': 20, 'date_posted': '2011-01-01'},
+        {'min_salary': 1, 'max_salary': 10, 'date_posted': '2010-01-01'},
+    ]
+
+    sort_by(fake_jobs, 'min_salary')
+    assert fake_jobs == expected_min
+
+    sort_by(fake_jobs, 'max_salary')
+    assert fake_jobs == expected_max
+
+    sort_by(fake_jobs, 'date_posted')
+    assert fake_jobs == expected_date
+
+    with pytest.raises(ValueError):
+        sort_by(fake_jobs, 'wrong')
