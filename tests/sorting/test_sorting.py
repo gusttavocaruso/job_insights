748e1ab140dc47bd4caf42ace66afb6750aa3980
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-03-23 19:26:23 -0300

    add 10.1th requirement maybe solved.. local-test shows: xfailed, like README asks

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    fake_criteria = 'min_salary'
+    fake_jobs = [
+        {'min_salary': 1, 'max_salary': 10},
+        {'min_salary': 2, 'max_salary': 20},
+        {'min_salary': 4, 'max_salary': 40},
+        {'min_salary': 3, 'max_salary': 30},
+    ]
+
+    expected_jobs = [
+        {'min_salary': 1, 'max_salary': 10},
+        {'min_salary': 2, 'max_salary': 20},
+        {'min_salary': 3, 'max_salary': 30},
+        {'min_salary': 4, 'max_salary': 40},
+    ]
+
+    assert sort_by(fake_jobs, fake_criteria) == expected_jobs
